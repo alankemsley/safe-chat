aa0370fa7590ec2730d09bdb36aead60ea20c53d
Georgi Nenov <geoergi.nenov89@gmail.com>
2017-10-05 12:22:47 -0400

    server.js added

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-
+// Pull on Dependeincies 
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Set Handlebars as the view engine
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+// GET function 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/controller.js');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
 });
-
+// Creating a PORT
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
+// Creating a connect and disconnect function
 io.on('connection', function(socket){
     console.log('a user connected');
     socket.on('disconnect', function(){
