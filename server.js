67b02cca309ade42dc0184119eb45348bb74c428
awallathome <adam.wallis@me.com>
2017-10-05 20:44:07 -0400

    added basic socket.io code

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var mysql = require('mysql');
+
+user = [];
+connections = [];
+
+server.listen(process.env.PORT || 3001);
+  console.log("server running");
+  app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket){
+  connections.push(socket);
+  console.log("connected: %s sockets connected", connections.length);
+
+  //Disconnect
+  connections.splice(connections.indexOf(socket), 1);
+  console.log("Disconnected. Phew! %s sockets connected", connections.length);
+});
