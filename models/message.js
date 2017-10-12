8b58c1aaf658d84ac6f92ea5a64eb57b6dc4540e
Georgi Nenov <geoergi.nenov89@gmail.com>
2017-10-12 19:42:39 -0400

    Set up server socket.io logic

---
+++ models/message.js
@@ -0,0 +1,26 @@
+//specifies what to CRUD with the ORM queries...
+
+var orm = require("../config/orm");
+var ioConnect = require("../config/io");
+
+
+// this code specified how to make the render query
+var messageModel = {
+  getMessages: function(cb) {
+    // ioConnect.on("connection", function(socket) {
+    //   socket.emit("youreventname", {message: "Some data to emit"});
+    //   orm.all("messages", cb);
+    // });
+    console.log("hshshs");
+  },
+// this code specified how to post the new message
+  postMessage: function(userMessage, cb) {
+    // ioConnect.on("connection", function(socket) {
+    //   socket.emit("youreventname", {message: "Some data to emit"});
+    //   orm.all("messages", cb);
+    // });
+    orm.create(userMessage, cb);
+  },
+};
+
+module.exports = messageModel;
