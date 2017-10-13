6960bec7108ecf54b9ed1999e6040fefd7464e06
awallathome <adam.wallis@me.com>
2017-10-12 21:03:33 -0400

    ignore this one

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
-  <script>
-    $(function() {
-      var socket = io.connect();
+  $(function() {
+      var socket = io();
       var $messageForm = $('#messageForm');
       var $message = $('#message');
       var $chat = $('#chat');
@@ -9,33 +8,27 @@
       var $messageArea = $('#messageArea');
       var $users = $('#users');
       var $username = $('#username');
-
-      $messageForm.submit(function(e) {
-        e.preventDefault();
-        socket.emit('send message', $message.val());
-        $message.val('');
-      });
-
-      socket.on('new message', function(data) {
-        $chat.append('<div class = "well"><strong>' + data.user + '</strong>:' + data.msg + '<div>');
-      });
+      var username = null;
 
       $userForm.submit(function(e) {
         e.preventDefault();
-        socket.emit('new user', $username.val(), function(data) {
-          if (data) {
-            $userFormArea.hide();
-            $messageArea.show();
-          }
-        });
-        $username.val('');
+        username = $username.val();
+        $username.val("");
       });
-      socket.on('get users', function(data) {
-        var html = '';
-        for (i = 0; i < data.length; i++) {
-          html += '<li class = "list-group-item" >' + data[i] + '</li>';
-        }
-        $users.html(html);
+
+      $messageForm.submit(function(e) {
+        e.preventDefault();
+
+        if (!username) return;
+
+        var message = $message.val();
+        $message.val("");
+
+        socket.emit('message', { username:  username, message: message });
       });
+
+      socket.on('message', function(userMessage) {
+        $chat.append('<div class = "well"><strong>' + userMessage.username + '</strong>:' + userMessage.message + '<div>');
+      });
+
     });
-  </script>
