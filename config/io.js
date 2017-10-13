2bb455d679f6952443826942a0f28c1f0d426fb4
awallathome <adam.wallis@me.com>
2017-10-12 20:25:26 -0400

    unsure

--- config/io.js
+++ config/io.js
@@ -6,9 +6,9 @@ var io = require('socket.io');
 module.exports = function (http) {
   var ioConnect = io(http);
   ioConnect.on('connection', function (socket) {
-    console.log('a user connected');
+    socket.on("message", function(message) {
+      
+    });
   });
-  
   return ioConnect;
-  
 }
