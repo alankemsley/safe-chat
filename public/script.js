6b3b889a6a078f141806e23ed6fe95203ac0ef92
awallathome <adam.wallis@me.com>
2017-10-14 10:26:23 -0400

    added jquery to script.js

--- public/script.js
+++ public/script.js
@@ -1,34 +1,39 @@
-  $(function() {
-      var socket = io();
-      var $messageForm = $('#messageForm');
-      var $message = $('#message');
-      var $chat = $('#chat');
-      var $userFormArea = $('#userFormArea');
-      var $userForm = $('#userForm');
-      var $messageArea = $('#messageArea');
-      var $users = $('#users');
-      var $username = $('#username');
-      var username = null;
+$(document).ready(function(){
+    var socket = io();
+    var $messageForm = $('#messageForm');
+    var $message = $('#message');
+    var $chat = $('#chat');
+    var $userFormArea = $('#userFormArea');
+    var $userForm = $('#userForm');
+    var $messageArea = $('#messageArea');
+    var $users = $('#users');
+    var $username = $('#username');
+    var username = null;
 
-      $userForm.submit(function(e) {
-        e.preventDefault();
-        username = $username.val();
-        $username.val("");
-      });
-
-      $messageForm.submit(function(e) {
-        e.preventDefault();
-
-        if (!username) return;
-
-        var message = $message.val();
-        $message.val("");
-
-        socket.emit('message', { username:  username, message: message });
-      });
-
-      socket.on('message', function(userMessage) {
-        $chat.append('<div class = "well"><strong>' + userMessage.username + '</strong>:' + userMessage.message + '<div>');
-      });
-
+    $userForm.submit(function(e) {
+      e.preventDefault();
+      username = $username.val();
+      $username.val("");
     });
+
+    $messageForm.submit(function(e) {
+      e.preventDefault();
+
+      if (!username) return;
+
+      var message = $message.val();
+      $message.val("");
+
+      socket.emit('message', { username:  username, message: message });
+    });
+
+    socket.on('message', function(userMessage) {
+      $chat.append('<div class = "well"><strong>' + userMessage.username + '</strong>:' + userMessage.message + '<div>');
+    });
+
+    $('#sendMessage').click(function(){
+      console.log("message was submitted");
+      var socket = io();
+      socket.emit("message", { username: $username.val(), message: $message.val()});
+    });
+});
