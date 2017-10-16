3779597ce471b0ffd7d383209aee856305c9f699
Alan Kemsley <alanjkemsley@gmail.com>
2017-10-16 19:53:12 -0400

    Add JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,22 @@
 var mysql = require("mysql");
 
 // Connect to MySQL
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "safechat"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "safechat"
+  });
+};
 
-connection.connect(function(err){
+connection.connect((function(err){
   if(err) {throw err;}
-  console.log("Connected to database on port 3306.");
-});
+  console.log("Connected to database.");
+}));
 
 // Export
 module.exports = connection;
