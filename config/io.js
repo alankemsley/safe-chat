446531d38cd3ccb4739b77f7bf02f1095e714344
Alan Kemsley <alanjkemsley@gmail.com>
2017-10-16 17:41:03 -0400

    Make message area pretty and clean up code

--- config/io.js
+++ config/io.js
@@ -2,7 +2,7 @@
 var io = require("socket.io");
 var messageModel = require("../models/message");
 
-// socket.io emit function
+// Socket.io emit function
 module.exports = function (http) {
   var ioConnect = io(http);
   ioConnect.on("connection", function (socket) {
