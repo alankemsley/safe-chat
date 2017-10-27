76731185e38f62484ba04cc36d7b5f75d54bcc89
Alan Kemsley <alanjkemsley@gmail.com>
2017-10-27 18:17:48 -0400

    Change both toggles to ESC key

--- public/script.js
+++ public/script.js
@@ -2,14 +2,26 @@ $(document).ready(function(){
   // Confirmation message for testing
   console.log("Client-side JavaScript file (script.js) is properly linked. You may begin chatting.");
   
-  // Variables
+  // Variables and functions
   var socket = io();
   var messageForm = $('#messageForm');
   var message = $('#message');
   var chat = $('#messages');
   var messageArea = $('#messageArea');
   var users = $('#users');
-  
+  var dummyShown = true;
+  var showReal = function() {
+    $('#messageArea2').addClass("hide");
+    $('#messageArea').removeClass("hide");
+    dummyShown = false;
+    Materialize.toast("Now showing real messages.", 2000);
+  };
+  var showDummy = function() {
+    $('#messageArea2').removeClass("hide");
+    $('#messageArea').addClass("hide");
+    dummyShown = true;
+  };
+
   // Prompt for user's name
   var name = prompt("Please enter your name.");
 
@@ -20,9 +32,9 @@ $(document).ready(function(){
   var randomAnimal = randomAnimalArray[Math.floor(Math.random()*38)];
   if (name == null || name === "") {
     name = "Anonymous " + randomAnimal;
-    Materialize.toast("Welcome, " + name + " !", 2000);
+    Materialize.toast("Welcome, " + name + " ! Press ESC to go between real and fake messages.", 4000);
   } else {
-    Materialize.toast("Welcome, " + name + " !", 2000);
+    Materialize.toast("Welcome, " + name + " ! Press ESC to go between real and fake messages.", 4000);
   }
 
   // Append message to chat window when message is sent
@@ -30,22 +42,14 @@ $(document).ready(function(){
     chat.append('<p><h6>' + userMessage.username + '</h6>' + userMessage.message + '</p>');
   });
 
-  // Toggle real messages 
+  // Toggle between real and dummy messages
   document.addEventListener("keydown", function(e) {
     var key = e.keyCode ? e.keyCode : e.which;
-    if(key === 192) {
-        $('#messageArea2').addClass("hide");
-        $('#messageArea').removeClass("hide");
-    } 
-  });
-
-  // Toggle fake messages
-  document.addEventListener("keydown", function(e) {
-    var key = e.keyCode ? e.keyCode : e.which;
-    if(key === 27) {
-        $('#messageArea2').removeClass("hide");
-        $('#messageArea').addClass("hide");
-    } 
+    if(key === 27 && dummyShown === true) {
+      showReal();
+    } else if (key === 27 && dummyShown === false) {
+      showDummy();
+    }
   });
 
   // When user clicks SEND, send message to DB and clear input field
